510ff325ee017606edc837f06f99d8879b133a4e
isak-kang <wbsldj59@naver.com>
2025-03-28 12:03:02 +0900

    3.27

--- programmers/lv2/짝지어제거하기.py
+++ programmers/lv2/짝지어제거하기.py
@@ -1,18 +1,22 @@
+# 짝지어지면 삭제
+# 스택을 이용해 처리......
+# 못풀어서 도움받음.
+
 def solution(s):
     answer = -1
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')   
+    for i in range(len(s)):
+        stack = []
+        if not stack:
+            stack.append(s[i])
+        else:
+            if s[i] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+    if not stack:
+        answer = 1
+    else :
+        answer = 0   
 
     return answer
-
-
-s = "baabaa"
-
-
-for i in range(len(s)):
-    if s[i] == s[i+1]:
-        s[i]
-        s.pop(i+1)
-            
-
